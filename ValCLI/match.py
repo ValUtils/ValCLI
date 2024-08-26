8540dc7402996718d5ab46d00ad2a7ff86d162d2
PWall <34860495+PWall2222@users.noreply.github.com>
2024-08-26 14:53:09 +0200

    feat: select character

--- ValCLI/match.py
+++ ValCLI/match.py
@@ -1,9 +1,11 @@
 import typer
+from httpx import get
 from ValWrap import (
     get_current_game_player,
     get_pregame_player,
     post_current_game_quit,
-    post_pregame_quit
+    post_pregame_quit,
+    post_select_character,
 )
 
 from .user import UserName, user_get
@@ -16,6 +18,17 @@ def dodge(username: UserName):
     _, auth = user_get(username)
     match_player = get_pregame_player(auth)
     post_pregame_quit(auth, match_player['MatchID'])
+
+
+@match_typer.command()
+def select(username: UserName, agent: str):
+    _, auth = user_get(username)
+    match_player = get_pregame_player(auth)
+    agents = get("https://valorant-api.com/v1/agents").json()["data"]
+    agents = {agent["displayName"]: agent["uuid"] for agent in agents}
+    if agent not in agents:
+        return
+    post_select_character(auth, match_player['MatchID'], agents[agent])
 
 
 @match_typer.command()
